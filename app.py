e265032752cceb6f7e7fee4fac1b70d81d8adb3a
Jose Luis Cuéllar <jlcuellaracosta@gmail.com>
2025-11-26 16:42:46 -0500

    scatter plot and change to checkbox

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ import pandas as pd
 import plotly.express as px
 
 car_data = pd.read_csv("vehicles_us.csv")
-hist_button = st.button("Construir Histograma")
 
 st.header("Analisis exploratorio vehicles dataset")
 
-if hist_button:
+
+hist_checkbox = st.checkbox("Construir Histograma")
+scatter_checkbox = st.checkbox("Construir diagrama de dispersion ")
+if hist_checkbox:
     st.write("Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
     fig = px.histogram(car_data, x="odometer")
     st.plotly_chart(fig,use_container_width=True)
-    
+
+if scatter_checkbox:
+    st.write("Creacion de un diagrama de dispersion para el conjunto de datos de anuncios de venta de coches")
+    fig = px.scatter(car_data,x="odometer", y="price")
+    fig.show()
