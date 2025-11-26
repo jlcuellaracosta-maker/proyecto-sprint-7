1de788a5c3fec750787f23d18f0bac1173b2fb6d
Jose Luis Cuéllar <jlcuellaracosta@gmail.com>
2025-11-26 16:28:51 -0500

    histogram

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv("vehicles_us.csv")
+car_data = pd.read_csv("vehicles_us.csv")
+hist_button = st.button("Construir Histograma")
 
 st.header("Analisis exploratorio vehicles dataset")
+
+if hist_button:
+    st.write("Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
+    fig = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig,use_container_width=True)
+    
